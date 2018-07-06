75c323ff15491160ed2edb0e7b7d91f8387e67e2
fanzhang <fanzhang@yunnex.com>
2018-07-06 11:03:22 +0800

    kotlin学习

---
+++ src/main/java/KotlinJava.java
@@ -0,0 +1,14 @@
+public class KotlinJava {
+
+    public static void vid(){
+
+        for(int i = 2; i >= 0; --i){
+            System.out.println("i= " + i);
+        }
+
+    }
+
+    public static void main(String ... args){
+        vid();
+    }
+}
